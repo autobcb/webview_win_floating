020c47efd7961d223413480ad8693397e8ec5e10
q9uo11 <q9uo11@qq.com>
2025-04-11 03:26:23 +0800

    新增setCookies接口

--- windows/my_webview.cpp
+++ windows/my_webview.cpp
@@ -699,7 +699,13 @@ HRESULT MyWebViewImpl::setCookies(LPCWSTR url, LPCWSTR cookies) {
         }
     }
 
-    // Add '.' to domain for subdomain compatibility
+    // 提取上级域名
+    size_t last_dot = domain.rfind(L'.', domain.rfind(L'.') - 1);
+    if (last_dot != std::wstring::npos) {
+        domain = domain.substr(last_dot);
+    }
+
+    // 在前面加上'.'
     if (!domain.empty() && domain[0] != L'.') {
         domain = L"." + domain;
     }
