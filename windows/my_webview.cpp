0b7b0be39436bd34a9f0d07eaa03c9d5aafc5011
q9uo11 <q9uo11@qq.com>
2025-04-11 04:04:30 +0800

    新增setCookies接口

--- windows/my_webview.cpp
+++ windows/my_webview.cpp
@@ -709,7 +709,7 @@ HRESULT MyWebViewImpl::setCookies(LPCWSTR url, LPCWSTR cookies) {
     if (!domain.empty() && domain[0] != L'.') {
         domain = L"." + domain;
     }
-    HRESULT hr 
+    HRESULT hr ;
 
     // Log domain
     std::cout << "[webview] Domain: " << utf8_encode(domain) << std::endl;
@@ -747,7 +747,7 @@ HRESULT MyWebViewImpl::setCookies(LPCWSTR url, LPCWSTR cookies) {
             std::cout << "[webview] Setting cookie: " << utf8_encode(name) << "=" << utf8_encode(value) << std::endl;
             
             wil::com_ptr<ICoreWebView2Cookie> cookie;
-            HRESULT hr = cookieManager->CreateCookie(name.c_str(), value.c_str(), domain.c_str(), L"/", &cookie);
+            hr = cookieManager->CreateCookie(name.c_str(), value.c_str(), domain.c_str(), L"/", &cookie);
             if (SUCCEEDED(hr)) {
                 // Set cookie properties
                 cookie->put_IsHttpOnly(FALSE);
